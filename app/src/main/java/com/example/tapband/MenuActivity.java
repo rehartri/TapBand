0110409aa87d948480f3fe0e1434ecec82f1fef9
n-wiszowaty <43485233+n-wiszowaty@users.noreply.github.com>
2020-04-13 10:50:29 -0500

    menu colors change

--- app/src/main/java/com/example/tapband/MenuActivity.java
+++ app/src/main/java/com/example/tapband/MenuActivity.java
@@ -5,11 +5,18 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.os.Debug;
+import android.util.Log;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 
 public class MenuActivity extends AppCompatActivity {
 
@@ -19,11 +26,17 @@ public class MenuActivity extends AppCompatActivity {
     Button chooseInstrumentButton;
     Button helpButton;
     ImageView instrumentImageView;
+    Button changeColorButton;
+
+    ImageView logoImage;
+    TextView logoText;
+
     int currentImage = 0;  //Determines the image when selecting an instrument on the menu as well as the type of instrument created in the main activity
+    int currentColor = 0;
 
     @SuppressLint("SourceLockedOrientationActivity")
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);//Do not touch
         setContentView(R.layout.activity_menu);//Do not touch
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
@@ -35,9 +48,29 @@ public class MenuActivity extends AppCompatActivity {
         chooseInstrumentButton = findViewById(R.id.ChooseInstrument);
         instrumentImageView = findViewById(R.id.instrumentView);
         helpButton = findViewById(R.id.helpButton);
+        changeColorButton = findViewById(R.id.colorButton);
+        logoImage = findViewById(R.id.logoBar);
+        logoText = findViewById(R.id.textView);
 
         final int[] drawables = new int[] { R.drawable.piano, R.drawable.synth}; // Put images here
 
+        final int[][] colorArray = new int[][] { { R.color.colorPrimary, R.color.ButtonColor, R.color.ButtonColor2, R.color.MenuButtons }, {R.color.RedPrimary, R.color.Red, R.color.RedDark, R.color.RedLight}, {R.color.GrayPrimary ,R.color.Gray, R.color.GrayDark, R.color.GrayLight} } ;
+
+        changeColorButton.setOnClickListener(new View.OnClickListener() {
+                  @Override
+                  public void onClick(View v) {
+                    currentColor++;
+                    if (currentColor >= colorArray.length)
+                    {
+                        currentColor = 0;
+                    }
+                    logoImage.setImageResource(colorArray[currentColor][0]);
+                    logoText.setTextColor(colorArray[currentColor][3]);
+                    keyboardIntent.putExtra("colors", colorArray[currentColor]);
+                  }
+             }
+
+        );
 
         //Button click listener -- aka this gets called when the continue button is pressed
         nextActivityButton.setOnClickListener(new View.OnClickListener() {
